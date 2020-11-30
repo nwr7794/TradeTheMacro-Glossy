2b548534fe0382f7a04295d109f4dcebee8097c9
nwr7794 <nwr7794@gmail.com>
2020-11-30 12:50:02 -0500

    Added Blog

---
+++ assets/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-2_TzTDMD90Y', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'kenbj20ebd0c5fug805eougug', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
