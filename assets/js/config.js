e9c66feec9ee51fc205326ddfdde6f3a63cf8e29
nwr7794 <nwr7794@gmail.com>
2020-12-03 11:59:27 -0500

    Added user system

--- assets/js/config.js
+++ assets/js/config.js
@@ -3,6 +3,7 @@ window._config = {
         userPoolId: 'us-east-2_TzTDMD90Y', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: 'kenbj20ebd0c5fug805eougug', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
+        // ,authenticationFlowType: 'USER_PASSWORD_AUTH' // Test
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
